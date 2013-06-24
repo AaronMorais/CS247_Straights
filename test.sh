8d0b7ac0bc5afbedb976b4dd2f85baacee01b79e
Aaron Morais <aaron.m@live.ca>
2013-06-23 20:08:23 -0400

    Update test script

--- test.sh
+++ test.sh
@@ -3,25 +3,47 @@
 createTestCommands () {
     rm commands
     touch commands
-    echo "c" >> commands
-    echo "c" >> commands
-    echo "c" >> commands
-    echo "c" >> commands
+	if [ ${1} == "All_Computer" ]
+	then
+	    echo "c" >> commands
+	    echo "c" >> commands
+	    echo "c" >> commands
+	    echo "c" >> commands
+	fi
+	if [ ${1} == "Deck_Ragequit" ]
+	then
+	    echo "h" >> commands
+	    echo "c" >> commands
+	    echo "c" >> commands
+	    echo "c" >> commands
+	    echo "deck" >> commands
+	    echo "ragequit" >> commands
+	fi
 }
 
+runTest() {
 #create ourOutput file for our code
 g++ Straights.cpp -o straights
-createTestCommands
+createTestCommands ${2}
 ./straights ${1} > ourOutput < commands
 
+scp commands aemorais@linux032.student.cs.uwaterloo.ca:cs247/commands
 #create providedOutput file for given binary
 ssh -t -t aemorais@linux032.student.cs.uwaterloo.ca << EOF
 cd cs247
-createTestCommands
 ./straights ${1} > providedOutput < commands
 logout
 EOF
 
 #diff both providedOutput and ourOutput file
 scp aemorais@linux032.student.cs.uwaterloo.ca:cs247/providedOutput providedOutput
-diff ourOutput providedOutput
+diff ourOutput providedOutput >> testResults
+}
+
+for number in {1..10000}
+do
+	rm testResults
+	touch testResults
+	runTest number "All_Computer"
+	runTest number "Deck_Ragequit"
+done
