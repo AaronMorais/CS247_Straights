400b0f13f0e8874af12dba438b53a1aff30f41af
Aaron Morais <aaron.m@live.ca>
2013-07-21 17:25:23 -0400

    Remove seed from main class. It is set in the seed dialogue

--- main.cpp
+++ main.cpp
@@ -4,12 +4,6 @@
 #include <cstdlib>
 
 int main(int argc, char *argv[]) {
-	int seed = 0;
-	if(argc > 1) {
-		seed = atoi(argv[1]);
-	}  
-    srand48(seed);
-
 	Gtk::Main   kit( argc, argv );          
 	MainWindow window;
 	Gtk::Main::run( window );
