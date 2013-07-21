812c46f940fb72ea6c09d1ec7e9032cc9199c400
Aaron Morais <aaron.m@live.ca>
2013-07-21 14:46:28 -0400

    Update TODO with and build script

--- run.sh
+++ run.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-make clean
-cd ../
 rsync -r -a -v -e ssh --delete ~/se/cs247/p1 cs:~/CS247Project
-ssh -X cs "cd CS247Project/p1; make clean; make; ./straights"
+ssh -X cs "cd CS247Project/p1; make; ./straights"
